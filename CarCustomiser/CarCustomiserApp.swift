0e9f0fe088e67e045663b8ab32170f8f20434d0e
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-01-16 20:28:22 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Shanahan, Krystian (JD) on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCustomiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarCustomiser
+//
+//  Created by Shanahan, Krystian (JD) on 16/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
