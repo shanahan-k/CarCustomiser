da5ad126ddff3500a899416af3e1b7fd2e67701b
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-01-20 11:26:14 +0000

    finished Part 2

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    let car =  Car(make:"Mazda",model:"mx5",topSpeed: 125, acceleration: 7.7, handling: 5)
+    @State private var starterCars = StarterCars()
+    @State private var selectedCar : Int = 0
+    @State private var exhaustPackage = false
+    @State private var tirePackage = false
     
     var body: some View {
-        Text(car.displayStats())
+        
+        let exhaustPackageBinding = Binding<Bool> (
+            get : {self.exhaustPackage},
+            set: {
+            newValue in self.exhaustPackage = newValue
+                if newValue {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                }
+                else{
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                    
+                }
+            }
+        )
+        let tirePackageBinding = Binding<Bool> (
+            get : {self.tirePackage},
+            set: {
+                newVal in self.tirePackage = newVal
+                if newVal {
+                    starterCars.cars[selectedCar].handling += 2
+                }
+                else {
+                    starterCars.cars[selectedCar].handling -= 2
+                }
+            }
+        )
+
+        
+        Form {
+        VStack(spacing:20){
+            Text(starterCars.cars[selectedCar].displayStats())
             .padding()
+            Button("Next Car", action:{
+                selectedCar = (selectedCar + 1) % self.starterCars.cars.count
+            })
+        }
+            Section {
+                Toggle("exhaust package" , isOn: exhaustPackageBinding)
+                Toggle("Tires Package" , isOn : tirePackageBinding)
+            }
+            
+            
+        
+        
+    }
     }
 }
 
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,12 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by Shanahan, Krystian (JD) on 20/01/2022.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make:"Mazda",model:"mx5",topSpeed: 125, acceleration: 7.7, handling: 5), Car(make:"Volkswagen",model:"Golf",topSpeed: 100, acceleration: 5.7, handling: 2), Car(make:"Honda",model:"Civic",topSpeed: 85, acceleration: 3, handling: 2), Car(make:"Pagani",model:"Zonda",topSpeed: 205, acceleration: 8.9, handling: 7)]
+}
